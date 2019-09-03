46d67471b2ecd5f942f19ac57d97c8fc47b23240
cwt2030 <cwt2030@gmail.com>
2019-09-03 08:57:55 +0300

    js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ var y =4
 var m =x*y
 
 document.write("العدد ٥*٤ <br> يساوي"+ "<br>"+ m);
+document.write("Hello, I will go to next line<br>")
+document.write("now I am in the new line<br>")
 
+function _test(){
+    document.write("welcom to my first function ")
+}
+_test()
 
 
