502ebd76f12932b58d2e5c45c1ef2f27d542c8e8
cwt2030 <cwt2030@gmail.com>
2019-09-10 19:46:30 +0300

    addDay 10

--- script.js
+++ script.js
@@ -30,8 +30,14 @@ var a = 2 ; var b =2 ; var c = a+b;
 document.write(c)
 */
 /////Day number 9 (  Math.max(x,y,z) // وسيط اكبر / من العمليات الرياضية)
-
+/*
 var x=10 ;y=20 ;z=30
 
 var a = Math.max(x,y,z)
+document.write(a)
+*/
+/////Day number 10 ( )Dates and Times( والوقت التاريخ)
+
+document.write("date & time  <br>")
+var a=  oldDateWithTime = new Date()
 document.write(a)
